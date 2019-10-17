18abe144458cecae6896f769ab71857a9d8ebce7
Adam Peace <adam.n.peace@gmail.com>
2019-10-17 14:41:14 +0100

    Adds working server response

--- ncsdns.py
+++ ncsdns.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 
+import pprint
+import struct
 from copy import copy
 from optparse import OptionParser, OptionValueError
-import pprint
-from random import seed, randint
-import struct
+from os.path import expandvars
+from random import randint, seed
 from socket import *
-from sys import exit, maxsize as MAXINT
-from time import time, sleep
+from sys import exit
+from sys import maxsize as MAXINT
+from time import sleep, time
 
 from libs.collections_backport import OrderedDict
-from libs.dnslib.RR import *
 from libs.dnslib.Header import Header
 from libs.dnslib.QE import QE
+from libs.dnslib.RR import *
 from libs.inetlib.types import *
 from libs.util import *
 
@@ -161,39 +163,125 @@ def ncsdns():
     )
     (options, args) = parser.parse_args()
 
-    ################################
-
+    # Create a client socket on which to send requests to other DNS
+    # servers:
     setdefaulttimeout(TIMEOUT)
     cs = socket(AF_INET, SOCK_DGRAM)
 
-    domain = DomainName("www.google.com")
+    # Create a server socket to accept incoming connections from DNS
+    # client resolvers (stub resolvers):
+    ss = socket(AF_INET, SOCK_DGRAM)
+    # Set timeout so no reply exceeds 60s TODO: Add message if failed
 
-    # outHeader = Header(20001, 0, 0, qr=0, qdcount=1)
-    outHeader = Header(
-        19876,
-        0,
-        0,
-        qdcount=1,
-        ancount=0,
-        nscount=0,
-        arcount=0,
-        qr=False,
-        aa=False,
-        tc=False,
-        rd=False,
-        ra=False,
-    )
-    outQuestion = QE(dn=domain, type=1)
-    print("a", hexdump(outHeader.pack()))
-    # print("b", hexdump(outQuestion.pack()))
-    payload = b"".join([outHeader.pack(), outQuestion.pack()])
+    ss.bind(("127.0.0.1", options.port))
+    serveripaddr, serverport = ss.getsockname()
+    ss.settimeout(60)
+    # NOTE: In order to pass the test suite, the following must be the
+    # first line that your dns server prints and flushes within one
+    # second, to sys.stdout:
+    print("%s: listening on port %d" % (sys.argv[0], serverport))
+    sys.stdout.flush()
 
-    print("Sent", hexdump(payload))
-    cs.sendto(payload, (ROOTNS_DN, 53))
-    data = cs.recvfrom(512)
-    print("Recvd: ", hexdump(data[0]))
-    print(data)
-    cs.close()
+    # Main Server Loop
+    while 1:
+        (data, client_address) = ss.recvfrom(512)  # DNS limits UDP msgs to 512 bytes
+
+        if not data and not Header.fromData(data)._qdcount == 1:
+            log.error("client provided no data")
+            continue
+
+        queryHeader = Header.fromData(data)
+        queryQuestion = QE.fromData(data, offset=12)
+
+        def getAnswer(domain, queryId):
+            def addToCache(domain, queryId):
+                cReqHeader = Header(
+                    queryId,
+                    opcode=Header.OPCODE_QUERY,
+                    rcode=Header.RCODE_NOERR,
+                    qr=0,  # 1 -response, 0=query
+                    qdcount=1,  # Questions #
+                    nscount=0,  # NS Entries #
+                    ancount=0,  # Answer #
+                    arcount=0,  # Addition #
+                    aa=False,  # Autho
+                    tc=False,  # Truncation
+                    rd=False,  # Recursion Desired
+                    ra=False,  # Recursion Available
+                )
+                cReqQuestion = QE(dn=domain, type=QE.TYPE_A)
+                payload = cReqHeader.pack() + cReqQuestion.pack()
+                cs.sendto(payload, (ROOTNS_IN_ADDR, 53))
+
+                data = cs.recvfrom(512)[0]
+
+                def getRecordsFromData(data):
+                    resNS = []
+                    resA = []
+                    headerLen = len(Header.fromData(data))
+                    questionLen = len(QE.fromData(data, offset=headerLen))
+                    offset = headerLen + questionLen
+                    while offset < len(data):
+                        curRR = RR.fromData(data, offset=offset)
+
+                        def processRR(curRR):
+                            print(curRR)
+                            if type(curRR) == RR_NS:
+                                resNS.append(curRR)
+                            elif type(curRR) == RR_A:
+                                resA.append(curRR)
+                            elif type(curRR) == RR_AAAA:
+                                pass
+                            else:
+                                logger.log(DEBUG2, "Unknown RR: {}".format(curRR))
+
+                        processRR(curRR[0])
+
+                        offset += curRR[1]
+                        # print("offset: {}/{}".format(offset, dataPayloadLen))
+                    return resNS, resA
+
+                csResNS, csResA = getRecordsFromData(data)
+
+                for rr in csResNS:
+                    print("Cached NS {}".format(rr.__str__()))
+                    nscache.put(rr._dn, rr._nsdn, (time() + rr._ttl), True)
+                for rr in csResA:
+                    print("Cached A  {}".format(rr.__str__()))
+                    acache.put(
+                        rr._dn, rr._addr, (time() + rr._ttl), authoritative=False
+                    )
+
+            addToCache(domain, queryId)
+
+        getAnswer(queryQuestion._dn, queryHeader._id)
+
+        def sendEmptyRes(domain):
+            resHeader = Header(
+                queryHeader._id,
+                opcode=Header.OPCODE_QUERY,
+                rcode=Header.RCODE_NOERR,
+                qr=1,  # 1 -response, 0=query
+                qdcount=1,  # Questions #
+                nscount=1,  # NS Entries #
+                ancount=0,  # Answer #
+                arcount=0,  # Addition #
+                aa=False,  # Autho
+                tc=False,  # Truncation
+                rd=False,  # Recursion Desired
+                ra=False,  # Recursion Available
+            )
+            resQuestion = QE(type=QE.TYPE_A, dn=domain)
+            reply = RR_NS(
+                DomainName("org."), 172800, DomainName("b0.org.afilias-nst.org.")
+            )
+            payload = resHeader.pack() + resQuestion.pack() + reply.pack()
+            logger.log(DEBUG2, "our reply in full:")
+            logger.log(DEBUG2, hexdump(payload))
+            ss.sendto(payload, client_address)
+
+        sendEmptyRes(queryQuestion._dn)
+        print("Done")
 
 
 if __name__ == "__main__":
